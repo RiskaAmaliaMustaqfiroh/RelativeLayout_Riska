171557f083767d817c4a6f837217452f772887ae
profile-guru <andoali385@gmail.com@email.com>
2023-10-23 14:16:31 +0700

    Initial commit

---
+++ app/src/main/java/com/mz/relativelayout_riska/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mz.relativelayout_riska
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
